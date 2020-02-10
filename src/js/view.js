f8ba1914977f6a28c23c48867424832d09c5c46a
J. Christian Romero <jcromerohdz@gmail.com>
2020-02-10 13:14:16 -0800

    Initial style setup

---
+++ src/js/view.js
@@ -0,0 +1,9 @@
+const header = () => {
+    const appName = () => {
+      //Do something
+        let content = document.querySelector('content');
+        content.innerHTML = '<div class="container"> <div>';
+    };
+
+  return {test3}  
+}
